e197fb4171d9a2feb6840bd1c3e6433e398207c8
nick <andantej281@gmail.com>
2025-02-09 09:36:16 +0900

    chore: project init

---
+++ src/main/kotlin/com/ssak3/timeattack/TimeAttackApplication.kt
@@ -0,0 +1,11 @@
+package com.ssak3.timeattack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeAttackApplication
+
+fun main(args: Array<String>) {
+    runApplication<TimeAttackApplication>(*args)
+}
---
+++ src/test/kotlin/com/ssak3/timeattack/TimeAttackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ssak3.timeattack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeAttackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
