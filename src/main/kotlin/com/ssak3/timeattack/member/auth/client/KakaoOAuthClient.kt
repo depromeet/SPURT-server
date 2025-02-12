923d93bb023ec3986294d1a0295c8b064ef9b20c
nick <andantej281@gmail.com>
2025-02-12 19:46:40 +0900

    feat: oauth client 추상화 개발 완료 - provider에 따라 알맞은 Feign Client를 반환하기 위한 추상화 진행

---
+++ src/main/kotlin/com/ssak3/timeattack/member/auth/client/KakaoOAuthClient.kt
@@ -0,0 +1,29 @@
+package com.ssak3.timeattack.member.auth.client
+
+import com.ssak3.timeattack.member.auth.properties.KakaoProperties
+import com.ssak3.timeattack.member.auth.oidc.OIDCPublicKeyList
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class KakaoOAuthClient(
+    @Autowired
+    val kakaoFeignClient: KakaoFeignClient,
+
+    @Autowired
+    val kakaoProperties: KakaoProperties,
+) : OAuthClient {
+
+    override fun getToken(authCode: String): OAuthTokenResponse {
+        return kakaoFeignClient.getToken(
+            code = authCode,
+            clientId = kakaoProperties.clientId,
+            clientSecret = kakaoProperties.clientSecret,
+            redirectUri = kakaoProperties.redirectUri,
+        )
+    }
+
+    override fun getPublicKeys(): OIDCPublicKeyList {
+        return kakaoFeignClient.getPublicKeys()
+    }
+}
---
+++ src/main/kotlin/com/ssak3/timeattack/member/auth/client/OAuthClient.kt
@@ -0,0 +1,8 @@
+package com.ssak3.timeattack.member.auth.client
+
+import com.ssak3.timeattack.member.auth.oidc.OIDCPublicKeyList
+
+interface OAuthClient {
+    fun getToken(authCode: String) : OAuthTokenResponse
+    fun getPublicKeys() : OIDCPublicKeyList
+}
