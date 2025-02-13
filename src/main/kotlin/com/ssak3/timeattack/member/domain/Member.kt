9e806db3af1ccf2183da0ec6f41a76ff3c44df8f
nick <andantej281@gmail.com>
2025-02-13 09:59:50 +0900

    style: import wildcard 제거

--- src/main/kotlin/com/ssak3/timeattack/member/domain/Member.kt
+++ src/main/kotlin/com/ssak3/timeattack/member/domain/Member.kt
@@ -1,6 +1,14 @@
 package com.ssak3.timeattack.member.domain
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.PreUpdate
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 
 @Entity
