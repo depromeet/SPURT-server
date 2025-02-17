2255f0a106ebf8100c26269873643c646f4ca6e2
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2025-02-17 20:22:36 +0900

    feat: 공통 에러 처리 코드 (#20)
    ### Jira Issue
    
    
    ### Proposed Changes
    - 공통에러처리 develop 에 반영
    
    ### Code Review Point

---
+++ src/main/kotlin/com/ssak3/timeattack/global/advice/GlobalControllerAdvice.kt
@@ -0,0 +1,24 @@
+package com.ssak3.timeattack.global.advice
+
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType
+import com.ssak3.timeattack.global.response.ExceptionResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalControllerAdvice {
+
+    @ExceptionHandler(ApplicationException::class)
+    fun handleApplicationException(exception: ApplicationException): ResponseEntity<ExceptionResponse> {
+        return ResponseEntity.status(exception.exceptionType.httpStatus).body(ExceptionResponse.from(exception))
+
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(exception: Exception): ResponseEntity<ExceptionResponse> {
+        val exceptionType = ApplicationExceptionType.UNDEFINED_EXCEPTION
+        return ResponseEntity.status(exceptionType.httpStatus).body(ExceptionResponse.from(exceptionType, exception.localizedMessage))
+    }
+}
