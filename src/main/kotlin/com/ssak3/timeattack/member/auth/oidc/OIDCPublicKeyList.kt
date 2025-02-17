73addf70a4e82e20ed8ca6342f0cc279d7b0ff7a
nick <andantej281@gmail.com>
2025-02-17 20:46:20 +0900

    feat: 주어진 kid(Key ID)와 alg(알고리즘)에 해당하는 OIDC 공개키를 찾아 반환하는 메서드 개발

--- src/main/kotlin/com/ssak3/timeattack/member/auth/oidc/OIDCPublicKeyList.kt
+++ src/main/kotlin/com/ssak3/timeattack/member/auth/oidc/OIDCPublicKeyList.kt
@@ -2,6 +2,8 @@ package com.ssak3.timeattack.member.auth.oidc
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType.OIDC_PUBLIC_KEY_NOT_FOUND
 import java.io.Serializable
 
 /**
@@ -11,7 +13,15 @@ import java.io.Serializable
 data class OIDCPublicKeyList(
     @JsonProperty("keys")
     val keys: List<OIDCPublicKey>
-) : Serializable
+) : Serializable {
+
+    /**
+     * 주어진 kid(Key ID)와 alg(알고리즘)에 해당하는 OIDC 공개키를 찾아 반환
+     */
+    fun getMatchedKey(kid: String, alg: String): OIDCPublicKey =
+        keys.find { it.kid == kid && it.alg == alg }
+            ?: throw ApplicationException(OIDC_PUBLIC_KEY_NOT_FOUND)
+}
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class OIDCPublicKey(
