36cd6d282706c03fbe8c68e271f1178a79d2bd9d
nick <andantej281@gmail.com>
2025-02-17 20:47:40 +0900

    feat: JWT 및 OIDC 에러 타입 추가

--- src/main/kotlin/com/ssak3/timeattack/global/exception/ApplicationExceptionType.kt
+++ src/main/kotlin/com/ssak3/timeattack/global/exception/ApplicationExceptionType.kt
@@ -19,6 +19,17 @@ enum class ApplicationExceptionType (
     BIND_EXCEPTION(HttpStatus.BAD_REQUEST, "ERR_GLOBAL_001", "Request 데이터 처리 중 오류가 발생했습니다. : {0}"),
 
     /**
+     * {0} : JWT 파싱 에러 메시지
+     */
+    JWT_INVALID_SIGNATURE(HttpStatus.BAD_REQUEST, "ERR_JWT_001", "잘못된 서명입니다. : {0}"),
+
+    /**
+     * {0} : JWT 파싱 에러 메시지
+     */
+    JWT_EXPIRED(HttpStatus.BAD_REQUEST, "ERR_JWT_002", "만료된 토큰입니다. : {0}"),
+
+    OIDC_PUBLIC_KEY_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "ERR_OIDC_001", "일치하는 공개키를 찾을 수 없습니다."),
+    /**
      * - {0} : Custom Exception Message
      */
     UNDEFINED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "ERR_GLOBAL_999", "정의되지 않은 에러입니다. : {0}");
