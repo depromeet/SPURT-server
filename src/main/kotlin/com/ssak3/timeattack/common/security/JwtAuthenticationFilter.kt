59af2235566fc64dc9c12b9f53b5d72e3caa961a
nick <andantej281@gmail.com>
2025-02-18 21:54:00 +0900

    feat: Jwt 인증 필터 개발 완료

--- src/main/kotlin/com/ssak3/timeattack/common/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/security/JwtAuthenticationFilter.kt
@@ -1,40 +1,53 @@
 package com.ssak3.timeattack.common.security
 
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType.JWT_NOT_FOUND
+import com.ssak3.timeattack.member.infrastructure.MemberRepository
+import com.ssak3.timeattack.member.infrastructure.findByIdOrThrow
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
+
 
 class JwtAuthenticationFilter(
     private val jwtTokenProvider: JwtTokenProvider,
+    private val memberRepository: MemberRepository,
 ) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        // 1. 쿠키에서 accessToken 가져오기
-        val accessToken = resolveToken(request)
+        // 쿠키에서 accessToken 가져오기
+        val accessToken = resolveToken(request) ?: throw ApplicationException(JWT_NOT_FOUND)
 
-        // 2. accessToken 유효성 검증
-        if (accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
+        // accessToken 유효성 검증
+        if (jwtTokenProvider.validateToken(accessToken)) {
             val memberId = jwtTokenProvider.getMemberIdFromToken(accessToken)
-
+            setAuthenticationInSecurityContext(memberId)
         }
-            // 2.1 유효 기간 지난 경우 -> 에러 던지기 -> 프론트에서는 refresh 토큰으로 accessToken 받는 api 호출
-        // 3. memberId 추출
-        // 4. SecurityContext에 저장
+
+        // 유효기간 지난 경우, getClaims()에서 JWT_EXPIRED 예외 발생
+
+        filterChain.doFilter(request, response)
     }
 
-    private fun resolveToken(request: HttpServletRequest) : String? {
+    private fun resolveToken(request: HttpServletRequest): String? {
         request.cookies?.forEach { cookie ->
             if (cookie.name == "accessToken") return cookie.value
         }
         return null
     }
 
+    // member 조회하고 securityContextHolder
     private fun setAuthenticationInSecurityContext(memberId: Long) {
+        val member = memberRepository.findByIdOrThrow(memberId)
+        val authenticationToken = UsernamePasswordAuthenticationToken(member, null, ArrayList())
 
+        SecurityContextHolder.getContext().authentication = authenticationToken
     }
 
 }
--- src/main/kotlin/com/ssak3/timeattack/member/infrastructure/MemberRepository.kt
+++ src/main/kotlin/com/ssak3/timeattack/member/infrastructure/MemberRepository.kt
@@ -1,9 +1,12 @@
 package com.ssak3.timeattack.member.infrastructure
 
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType.MEMBER_NOT_FOUND_BY_ID
 import com.ssak3.timeattack.member.domain.Member
 import com.ssak3.timeattack.member.domain.OAuthProvider
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.data.repository.query.Param
 
 interface MemberRepository : JpaRepository<Member, Long> {
@@ -14,3 +17,7 @@ interface MemberRepository : JpaRepository<Member, Long> {
         @Param("subject") subject: String
     ): Member?
 }
+
+fun MemberRepository.findByIdOrThrow(id: Long): Member =
+    findByIdOrNull(id) ?: throw ApplicationException(
+        MEMBER_NOT_FOUND_BY_ID)
