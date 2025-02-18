f0c5d5907fbb964829ddacb4fe2e0e3ec1cb810f
skydreamer <skydreamer210@gmail.com>
2025-02-19 02:17:32 +0900

    fix: Persona 도메인 및 엔티티 클래스에 키워드별 컬럼 추가

--- src/main/kotlin/com/ssak3/timeattack/persona/domain/Persona.kt
+++ src/main/kotlin/com/ssak3/timeattack/persona/domain/Persona.kt
@@ -3,7 +3,8 @@ package com.ssak3.timeattack.persona.domain
 class Persona (
     val id: Long,
     val name: String,
-    val personaImageUrl: String
+    val personaImageUrl: String,
+    val taskKeywordsCombination: TaskKeywordsCombination
 ) {
 
 
--- src/main/kotlin/com/ssak3/timeattack/persona/repository/entity/PersonaEntity.kt
+++ src/main/kotlin/com/ssak3/timeattack/persona/repository/entity/PersonaEntity.kt
@@ -1,14 +1,24 @@
 package com.ssak3.timeattack.persona.repository.entity
 
+import com.ssak3.timeattack.task.repository.entity.TaskTypeEntity
 import jakarta.persistence.Column
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 
 @Entity
-@Table(name = "persona")
+@Table(name = "persona",
+    uniqueConstraints = [
+        UniqueConstraint(name = "UK_TASK_TYPE__TASK_MODE", columnNames = ["task_type_id", "task_mode_id"])
+    ]
+)
 class PersonaEntity (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +29,14 @@ class PersonaEntity (
 
     @Column(name = "persona_image_url")
     val personaImageUrl: String,
+
+    @ManyToOne
+    @JoinColumn(name = "task_type_id", foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    val taskTypeEntity: TaskTypeEntity,
+
+    @ManyToOne
+    @JoinColumn(name = "task_mode_id", foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    val taskModeEntity: TaskTypeEntity
 ) {
 
 }
