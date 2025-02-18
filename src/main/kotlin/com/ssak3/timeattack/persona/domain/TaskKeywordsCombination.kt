ade158a025bfadbb67196f44664e742035f488d7
skydreamer <skydreamer210@gmail.com>
2025-02-19 02:47:58 +0900

    feat: TaskKeywordsCombination 클래스 작성

---
+++ src/main/kotlin/com/ssak3/timeattack/persona/domain/TaskKeywordsCombination.kt
@@ -0,0 +1,10 @@
+package com.ssak3.timeattack.persona.domain
+
+import com.ssak3.timeattack.task.domain.TaskMode
+import com.ssak3.timeattack.task.domain.TaskType
+
+class TaskKeywordsCombination(
+    val taskType: TaskType,
+    val taskMode: TaskMode,
+) {
+}
