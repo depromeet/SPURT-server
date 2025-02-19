6ee62a09e4a14bf340373a281e62b5f5ac8b6c60
nick <andantej281@gmail.com>
2025-02-19 17:47:14 +0900

    feat: 쿼리 로직 @Query -> queryDSL로 수정

--- src/main/kotlin/com/ssak3/timeattack/member/infrastructure/MemberRepository.kt
+++ src/main/kotlin/com/ssak3/timeattack/member/infrastructure/MemberRepository.kt
@@ -1,16 +1,6 @@
 package com.ssak3.timeattack.member.infrastructure
 
 import com.ssak3.timeattack.member.domain.Member
-import com.ssak3.timeattack.member.domain.OAuthProvider
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
-interface MemberRepository : JpaRepository<Member, Long> {
-
-    @Query("SELECT m FROM Member m WHERE m.oAuthProviderInfo.oauthProvider = :oauthProvider AND m.oAuthProviderInfo.subject = :subject")
-    fun findByProviderAndSubject(
-        @Param("oauthProvider") oauthProvider: OAuthProvider,
-        @Param("subject") subject: String
-    ): Member?
-}
+interface MemberRepository : JpaRepository<Member, Long>, MemberRepositoryCustom
