af5384dc0e2ca9ab00f84d51bc3c9f6a6d5214db
nick <andantej281@gmail.com>
2025-02-20 11:40:02 +0900

    refactor: val로 수정

--- src/main/kotlin/com/ssak3/timeattack/common/security/JwtTokenDto.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/security/JwtTokenDto.kt
@@ -1,6 +1,6 @@
 package com.ssak3.timeattack.common.security
 
 data class JwtTokenDto(
-    var accessToken: String,
-    var refreshToken: String,
+    val accessToken: String,
+    val refreshToken: String,
 )
