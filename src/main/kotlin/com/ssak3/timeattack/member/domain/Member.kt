6848060dafdd10298bdbe6ac2a5c44b0460dd523
nick <andantej281@gmail.com>
2025-02-24 18:32:37 +0900

    refactor: Member 도메인에 createdAt, updatedAt 컬럼 추가

--- src/main/kotlin/com/ssak3/timeattack/member/domain/Member.kt
+++ src/main/kotlin/com/ssak3/timeattack/member/domain/Member.kt
@@ -2,6 +2,7 @@ package com.ssak3.timeattack.member.domain
 
 import com.ssak3.timeattack.member.repository.entity.MemberEntity
 import com.ssak3.timeattack.member.repository.entity.OAuthProviderInfo
+import java.time.LocalDateTime
 
 data class Member(
     val id: Long? = null,
@@ -9,6 +10,8 @@ data class Member(
     val email: String,
     val profileImageUrl: String,
     val oAuthProviderInfo: OAuthProviderInfo,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
     val isDeleted: Boolean = false,
 ) {
     fun toEntity() =
@@ -29,6 +32,8 @@ data class Member(
                 email = memberEntity.email,
                 profileImageUrl = memberEntity.profileImageUrl,
                 oAuthProviderInfo = memberEntity.oAuthProviderInfo,
+                createdAt = memberEntity.createdAt,
+                updatedAt = memberEntity.updatedAt,
                 isDeleted = memberEntity.isDeleted,
             )
     }
