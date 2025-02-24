cc6e18348fcf140087a9d8020752fa70ae134284
nick <andantej281@gmail.com>
2025-02-25 00:10:11 +0900

    refactor: 클래스명 수정

--- src/main/kotlin/com/ssak3/timeattack/common/security/refreshtoken/RefreshTokenRedisRepository.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/security/refreshtoken/RefreshTokenRedisRepository.kt
@@ -4,6 +4,4 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RefreshTokenRedisRepository : CrudRepository<RefreshToken, String> {
-
-}
+interface RefreshTokenRedisRepository : CrudRepository<RefreshTokenEntity, String>
