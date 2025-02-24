ebc91b850b465b821d0aeb3e97f7c9de3dc3eed0
nick <andantej281@gmail.com>
2025-02-24 18:52:08 +0900

    refactor: 불필요한 컬럼 삭제

--- src/main/kotlin/com/ssak3/timeattack/common/domain/BaseEntity.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/domain/BaseEntity.kt
@@ -13,12 +13,9 @@ import java.time.LocalDateTime
 abstract class BaseEntity {
     @CreatedDate
     @Column(name = "created_at", updatable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now()
+    val createdAt: LocalDateTime = LocalDateTime.now()
 
     @LastModifiedDate
     @Column(name = "updated_at")
     var updatedAt: LocalDateTime = LocalDateTime.now()
-
-    @Column(name = "is_deleted")
-    var isDeleted: Boolean = false
 }
