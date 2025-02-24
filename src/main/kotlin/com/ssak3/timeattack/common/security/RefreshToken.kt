c95df06d34cb3bca60c93a2687010ebdb80e4c8e
nick <andantej281@gmail.com>
2025-02-24 23:06:48 +0900

    feat: Redis에 저장할 RefreshToken data class 생성

---
+++ src/main/kotlin/com/ssak3/timeattack/common/security/RefreshToken.kt
@@ -0,0 +1,11 @@
+package com.ssak3.timeattack.common.security
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+
+@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 7)
+data class RefreshToken(
+    @Id
+    val id: String,
+    val refreshToken: String,
+)
