6dcb9197cca01281e9b7eb857cb554b833215246
nick <andantej281@gmail.com>
2025-02-24 23:19:45 +0900

    refactor: 디렉토리 수정

--- src/main/kotlin/com/ssak3/timeattack/common/security/token/domain/RefreshToken.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/security/token/domain/RefreshToken.kt
@@ -1,4 +1,4 @@
-package com.ssak3.timeattack.common.security
+package com.ssak3.timeattack.common.security.token.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
