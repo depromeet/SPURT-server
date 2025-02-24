472cdce12abe7bd3ab33ecc17b0a8a895847bfff
nick <andantej281@gmail.com>
2025-02-25 00:10:36 +0900

    feat: access, refresh token 재발급 로직 개발

--- src/main/kotlin/com/ssak3/timeattack/common/security/refreshtoken/RefreshTokenService.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/security/refreshtoken/RefreshTokenService.kt
@@ -1,7 +1,50 @@
 package com.ssak3.timeattack.common.security.refreshtoken
 
+import com.ssak3.timeattack.common.security.JwtTokenDto
+import com.ssak3.timeattack.common.security.JwtTokenProvider
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType
 import org.springframework.stereotype.Service
 
 @Service
-class RefreshTokenService {
+class RefreshTokenService(
+    private val refreshTokenRedisRepository: RefreshTokenRedisRepository,
+    private val jwtTokenProvider: JwtTokenProvider,
+) {
+    /**
+     * refresh token을 이용하여 access token과 refresh token을 재발급한다.
+     * 재발급한 refresh token은 Redis에 저장한다.
+     */
+    fun reissueTokens(refreshToken: String): JwtTokenDto {
+        // refresh token 검증 & memberId 추출
+        val memberId = jwtTokenProvider.getMemberIdFromToken(refreshToken)
+
+        // Redis에서 refresh token 조회
+        val refreshTokenEntity =
+            refreshTokenRedisRepository.findById(memberId.toString())
+                .orElseThrow { ApplicationException(ApplicationExceptionType.JWT_REFRESH_NOT_FOUND_IN_REDIS) }
+
+        // refresh token 검증
+        if (!refreshTokenEntity.validateRefreshToken(refreshToken)) {
+            throw ApplicationException(ApplicationExceptionType.JWT_REFRESH_INVALID)
+        }
+
+        // 토큰 재발급
+        val tokens = jwtTokenProvider.generateTokens(memberId)
+
+        // refresh token 저장
+        saveRefreshToken(memberId, tokens.refreshToken)
+
+        return tokens
+    }
+
+    /**
+     * refresh token을 저장한다.
+     */
+    fun saveRefreshToken(
+        memberId: Long,
+        refreshToken: String,
+    ) {
+        refreshTokenRedisRepository.save(RefreshTokenEntity(memberId.toString(), refreshToken))
+    }
 }
