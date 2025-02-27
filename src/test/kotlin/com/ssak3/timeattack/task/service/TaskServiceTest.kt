49257f4e23b2e3516948b26ffbb12e50c02f5939
nick <andantej281@gmail.com>
2025-02-27 16:03:19 +0900

    test: 단위 테스트로 커버되는 테스트들 모두 삭제

--- src/test/kotlin/com/ssak3/timeattack/task/service/TaskServiceTest.kt
+++ src/test/kotlin/com/ssak3/timeattack/task/service/TaskServiceTest.kt
@@ -1,8 +1,5 @@
 package com.ssak3.timeattack.task.service
 
-import com.ssak3.timeattack.global.exception.ApplicationException
-import com.ssak3.timeattack.global.exception.ApplicationExceptionType
-import com.ssak3.timeattack.global.exception.ApplicationExceptionType.TASK_INVALID_STATE_TRANSITION
 import com.ssak3.timeattack.member.domain.Member
 import com.ssak3.timeattack.member.domain.OAuthProvider
 import com.ssak3.timeattack.member.repository.MemberRepository
@@ -10,13 +7,9 @@ import com.ssak3.timeattack.member.repository.entity.MemberEntity
 import com.ssak3.timeattack.member.repository.entity.OAuthProviderInfo
 import com.ssak3.timeattack.persona.repository.PersonaRepository
 import com.ssak3.timeattack.persona.repository.entity.PersonaEntity
-import com.ssak3.timeattack.task.controller.dto.TaskStatusRequest
 import com.ssak3.timeattack.task.controller.dto.UrgentTaskRequest
 import com.ssak3.timeattack.task.domain.TaskCategory
-import com.ssak3.timeattack.task.domain.TaskStatus
-import com.ssak3.timeattack.task.domain.TaskStatus.WARMING_UP
 import com.ssak3.timeattack.task.repository.TaskModeRepository
-import com.ssak3.timeattack.task.repository.TaskRepository
 import com.ssak3.timeattack.task.repository.TaskTypeRepository
 import com.ssak3.timeattack.task.repository.entity.TaskModeEntity
 import com.ssak3.timeattack.task.repository.entity.TaskTypeEntity
@@ -24,7 +17,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -41,8 +33,6 @@ class TaskServiceTest(
     @Autowired private val taskModeRepository: TaskModeRepository,
     @Autowired private val personaRepository: PersonaRepository,
 ) {
-    @Autowired
-    private lateinit var taskRepository: TaskRepository
     private lateinit var member: Member
 
     @BeforeEach
@@ -94,99 +84,5 @@ class TaskServiceTest(
         val savedTaskKeywordsCombination = task.persona.taskKeywordsCombination
         assertEquals(savedTaskKeywordsCombination.taskType.name, "프로그래밍")
         assertEquals(savedTaskKeywordsCombination.taskMode.name, "즐거운")
-    }
-
-    @Test
-    @DisplayName("Task 상태를 BEFORE에서 WARMING_UP으로 성공적으로 변경한다")
-    fun changeTaskStatusFromBeforeToWarmingUpTest() {
-        // given
-        val taskId = createTestTask()
-        val request = TaskStatusRequest(status = WARMING_UP)
-
-        // when
-        taskService.changeTaskStatus(taskId, checkNotNull(member.id), request)
-
-        // then
-        val updatedTask = taskRepository.findById(taskId).get()
-        assertEquals(WARMING_UP, updatedTask.status)
-        assertEquals(taskId, updatedTask.id)
-    }
-
-    @Test
-    @DisplayName("Task 상태를 한 단계 건너뛰어 BEFORE에서 FOCUSED로 성공적으로 변경한다")
-    fun changeTaskStatusSkippingOneStateTest() {
-        // given
-        val taskId = createTestTask()
-        val request = TaskStatusRequest(status = TaskStatus.FOCUSED)
-
-        // when
-        taskService.changeTaskStatus(taskId, checkNotNull(member.id), request)
-
-        // then
-        val updatedTask = taskRepository.findById(taskId).get()
-        assertEquals(TaskStatus.FOCUSED, updatedTask.status)
-        assertEquals(taskId, updatedTask.id)
-    }
-
-    @Test
-    @DisplayName("기존 상태보다 이전 상태로 변경할 경우 예외가 발생한다")
-    fun changeTaskStatusToPreviousStateThrowsExceptionTest() {
-        // given
-        val taskId = createTestTask()
-        val request = TaskStatusRequest(status = TaskStatus.FOCUSED)
-
-        taskService.changeTaskStatus(taskId, checkNotNull(member.id), request)
-
-        val invalidRequest = TaskStatusRequest(status = WARMING_UP)
-
-        // when & then: FOCUSED -> WARMING_UP 로 변경 시도
-        assertThrows<ApplicationException> {
-            taskService.changeTaskStatus(taskId, checkNotNull(member.id), invalidRequest)
-        }.apply {
-            assertEquals(this.exceptionType, TASK_INVALID_STATE_TRANSITION)
-        }
-    }
-
-    @Test
-    @DisplayName("다른 사용자가 Task 상태 변경을 시도하면 예외가 발생한다")
-    fun changeTaskStatusByAnotherMemberThrowsExceptionTest() {
-        // given
-        val taskId = createTestTask()
-        val otherMemberEntity =
-            MemberEntity(
-                nickname = "otherUser",
-                email = "other@test.com",
-                profileImageUrl = "https://test.com",
-                oAuthProviderInfo =
-                    OAuthProviderInfo(
-                        oauthProvider = OAuthProvider.KAKAO,
-                        subject = "9876543210",
-                    ),
-            )
-        val otherMember = Member.fromEntity(memberRepository.saveAndFlush(otherMemberEntity))
-        val request = TaskStatusRequest(status = WARMING_UP)
-
-        // when & then
-        assertThrows<ApplicationException> {
-            taskService.changeTaskStatus(taskId, checkNotNull(otherMember.id), request)
-        }.apply {
-            assertEquals(this.exceptionType, ApplicationExceptionType.TASK_MODIFICATION_NOT_ALLOWED_FOR_MEMBER)
-        }
-    }
-
-    /**
-     * 테스트용 Task를 생성하는 헬퍼 메서드
-     */
-    private fun createTestTask(taskName: String = "urgent task"): Long {
-        val taskRequest =
-            UrgentTaskRequest(
-                taskName,
-                LocalDateTime.now().plusDays(1),
-                "프로그래밍",
-                "즐거운",
-            )
-
-        val task = taskService.createUrgentTask(member, taskRequest)
-        return checkNotNull(task.id) { "Task ID는 생성 후 조회 시 null일 수 없습니다" }
     }
 }
