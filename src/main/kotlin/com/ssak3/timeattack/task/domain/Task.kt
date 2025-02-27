20e54fd73adbb556cbd67ecb5c1d5a10de519aa9
skydreamer <skydreamer210@gmail.com>
2025-02-27 19:00:53 +0900

    feat: 여유 있게 시작 작업 등록 API 서비스 로직 구현

--- src/main/kotlin/com/ssak3/timeattack/task/domain/Task.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/domain/Task.kt
@@ -1,5 +1,7 @@
 package com.ssak3.timeattack.task.domain
 
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType
 import com.ssak3.timeattack.member.domain.Member
 import com.ssak3.timeattack.persona.domain.Persona
 import com.ssak3.timeattack.task.repository.entity.TaskEntity
@@ -30,6 +32,25 @@ class Task(
             member = member.toEntity(),
             persona = persona.toEntity(),
         )
+
+    fun validateTriggerActionAlarmTime(triggerActionAlarmTime: LocalDateTime) {
+        // 현재 버퍼타임에 대한 계산을 프론트에서 해서 보여주기 때문에 서버에서는 정확한 버퍼타임에 대한 배수는 검증하지 않음
+        // triggerActionAlarmTime은 마감 이전이어야 한다.
+        // triggerActionAlarmTime부터 dueDatetime까지의 시간이 estimatedTime보다 커야 한다.
+
+        if (category == TaskCategory.URGENT) {
+            throw ApplicationException(ApplicationExceptionType.TASK_CATEGORY_MISMATCH, category)
+        }
+        val checkedEstimatedTime = (checkNotNull(estimatedTime) { "estimatedTime must not be null" }).toLong()
+        if (triggerActionAlarmTime.plusMinutes(checkedEstimatedTime).isAfter(dueDatetime)) {
+            throw ApplicationException(
+                ApplicationExceptionType.INVALID_TRIGGER_ACTION_ALARM_TIME,
+                triggerActionAlarmTime,
+                dueDatetime,
+                estimatedTime
+            )
+        }
+    }
 
     companion object {
         fun fromEntity(entity: TaskEntity) =
--- src/main/kotlin/com/ssak3/timeattack/task/service/TaskService.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/service/TaskService.kt
@@ -5,6 +5,7 @@ import com.ssak3.timeattack.global.exception.ApplicationExceptionType
 import com.ssak3.timeattack.member.domain.Member
 import com.ssak3.timeattack.persona.domain.Persona
 import com.ssak3.timeattack.persona.repository.PersonaRepository
+import com.ssak3.timeattack.task.controller.dto.ScheduledTaskCreateRequest
 import com.ssak3.timeattack.task.controller.dto.UrgentTaskRequest
 import com.ssak3.timeattack.task.domain.Task
 import com.ssak3.timeattack.task.domain.TaskCategory
@@ -12,6 +13,8 @@ import com.ssak3.timeattack.task.domain.TaskStatus
 import com.ssak3.timeattack.task.repository.TaskModeRepository
 import com.ssak3.timeattack.task.repository.TaskRepository
 import com.ssak3.timeattack.task.repository.TaskTypeRepository
+import com.ssak3.timeattack.task.service.events.ScheduledTaskSaveEvent
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -21,12 +24,15 @@ class TaskService(
     private val taskTypeRepository: TaskTypeRepository,
     private val taskModeRepository: TaskModeRepository,
     private val personaRepository: PersonaRepository,
+
+    private val eventPublisher: ApplicationEventPublisher,
 ) {
     @Transactional
     fun createUrgentTask(
         member: Member,
         urgentTaskRequest: UrgentTaskRequest,
     ): Task {
+        // TODO: 페르소나 가져오는 메서드 사용하기
         // 1. 키워드 검증 진행
         val taskTypeEntity = (
             taskTypeRepository.findByName(urgentTaskRequest.taskType)
@@ -69,5 +75,67 @@ class TaskService(
 
         // 5. Task 반환
         return Task.fromEntity(savedTaskEntity)
+    }
+
+    @Transactional
+    fun createScheduledTask(member: Member, scheduledTaskRequest: ScheduledTaskCreateRequest): Task {
+        // 1. Persona 가져오기
+        val persona = findPersonaByTaskTypeAndTaskMode(scheduledTaskRequest.taskType, scheduledTaskRequest.taskMode)
+
+        // 2. Task 생성
+        val task =
+            Task(
+                name = scheduledTaskRequest.name,
+                category = TaskCategory.SCHEDULED,
+                dueDatetime = scheduledTaskRequest.dueDatetime,
+                status = TaskStatus.BEFORE,
+                triggerAction = scheduledTaskRequest.triggerAction,
+                estimatedTime = scheduledTaskRequest.estimatedTime,
+                member = member,
+                persona = persona,
+            )
+
+        task.validateTriggerActionAlarmTime(scheduledTaskRequest.triggerActionAlarmTime)
+
+        // 3. Task 저장
+        val savedTaskEntity = taskRepository.save(task.toEntity())
+
+        // 4. Task 이벤트 발행
+        // savedTaskEntity id 값이 null이 될 수 없으므로 !! 연산자 사용
+        val scheduledTaskSaveEvent = ScheduledTaskSaveEvent(savedTaskEntity.id!!, savedTaskEntity.category, scheduledTaskRequest.triggerActionAlarmTime)
+        eventPublisher.publishEvent(scheduledTaskSaveEvent)
+
+        // 5. Task 반환
+        return Task.fromEntity(savedTaskEntity)
+
+    }
+
+    fun findPersonaByTaskTypeAndTaskMode(taskType: String, taskMode: String): Persona {
+        // 작업 유형 키워드 존재 검증
+        val taskTypeEntity = (
+            taskTypeRepository.findByName(taskType)
+                ?: throw ApplicationException(
+                    ApplicationExceptionType.TASK_TYPE_NOT_FOUND_BY_NAME,
+                    taskType,
+                )
+        )
+
+        // 작업 분위기 키워드 존재 검증
+        val taskModeEntity = (
+            taskModeRepository.findByName(taskMode)
+                ?: throw ApplicationException(
+                    ApplicationExceptionType.TASK_MODE_NOT_FOUND_BY_NAME,
+                    taskMode,
+                )
+        )
+
+        return Persona.fromEntity(
+            personaRepository.findByTaskTypeAndTaskMode(taskTypeEntity, taskModeEntity)
+                ?: throw ApplicationException(
+                    ApplicationExceptionType.PERSONA_NOT_FOUND_BY_TASK_KEYWORD_COMBINATION,
+                    taskType,
+                    taskMode,
+                ),
+        )
     }
 }
