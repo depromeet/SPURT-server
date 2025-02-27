afd8647bfb1723aacb1a82be011d6622c24c3555
nick <andantej281@gmail.com>
2025-02-27 15:23:23 +0900

    refactor: swagger description 추가

--- src/main/kotlin/com/ssak3/timeattack/task/controller/dto/TaskStatusRequest.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/controller/dto/TaskStatusRequest.kt
@@ -6,6 +6,11 @@ import io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED
 
 @Schema(description = "작업 상태 변경 요청")
 data class TaskStatusRequest(
-    @Schema(title = "변경할 상태", example = "WARMING_UP | FOCUSED | COMPLETE", requiredMode = REQUIRED)
+    @Schema(
+        title = "변경할 상태",
+        description = "클라이언트에서 BEFORE, FAIL로 상태를 변경할 수 없습니다.",
+        example = "WARMING_UP | FOCUSED | COMPLETE",
+        requiredMode = REQUIRED,
+    )
     val status: TaskStatus,
 )
