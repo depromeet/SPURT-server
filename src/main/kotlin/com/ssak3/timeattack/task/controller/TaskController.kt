907b86cf8a0eb968a18deeb25855168cc5212e3c
nick <andantej281@gmail.com>
2025-02-27 12:53:36 +0900

    feat: task 상태 수정 API 개발

--- src/main/kotlin/com/ssak3/timeattack/task/controller/TaskController.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/controller/TaskController.kt
@@ -1,7 +1,10 @@
 package com.ssak3.timeattack.task.controller
 
 import com.ssak3.timeattack.common.config.SwaggerConfig.Companion.SECURITY_SCHEME_NAME
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType.UNAUTHORIZED_ACCESS
 import com.ssak3.timeattack.member.domain.Member
+import com.ssak3.timeattack.task.controller.dto.UpdateTaskStatusRequest
 import com.ssak3.timeattack.task.controller.dto.UrgentTaskRequest
 import com.ssak3.timeattack.task.controller.dto.UrgentTaskResponse
 import com.ssak3.timeattack.task.service.TaskService
@@ -10,6 +13,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -28,5 +33,15 @@ class TaskController(
     ): ResponseEntity<UrgentTaskResponse> {
         val createdUrgentTask = taskService.createUrgentTask(member, urgentTaskRequest)
         return ResponseEntity.ok(UrgentTaskResponse.from(createdUrgentTask))
+    }
+
+    @PatchMapping("/{taskId}/status")
+    fun changeStatus(
+        @PathVariable(required = true) taskId: Long,
+        @AuthenticationPrincipal member: Member,
+        @RequestBody @Valid updateTaskStatusRequest: UpdateTaskStatusRequest,
+    ) {
+        checkNotNull(member.id) { throw ApplicationException(UNAUTHORIZED_ACCESS) }
+        taskService.changeTaskStatus(taskId, member.id, updateTaskStatusRequest)
     }
 }
---
+++ src/main/kotlin/com/ssak3/timeattack/task/controller/dto/UpdateTaskStatusRequest.kt
@@ -0,0 +1,11 @@
+package com.ssak3.timeattack.task.controller.dto
+
+import com.ssak3.timeattack.task.domain.TaskStatus
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED
+
+@Schema(description = "작업 상태 변경 요청")
+data class UpdateTaskStatusRequest(
+    @Schema(title = "변경할 상태", example = "WARMING_UP | FOCUSED | COMPLETE", requiredMode = REQUIRED)
+    val status: TaskStatus,
+)
