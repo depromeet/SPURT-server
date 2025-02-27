5f5af415e06cd38f37c23f91c40f4d59d7f0e2a6
nick <andantej281@gmail.com>
2025-02-27 12:19:41 +0900

    feat: task 상태 수정 로직 개발

--- src/main/kotlin/com/ssak3/timeattack/task/domain/Task.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/domain/Task.kt
@@ -1,5 +1,7 @@
 package com.ssak3.timeattack.task.domain
 
+import com.ssak3.timeattack.global.exception.ApplicationException
+import com.ssak3.timeattack.global.exception.ApplicationExceptionType
 import com.ssak3.timeattack.member.domain.Member
 import com.ssak3.timeattack.persona.domain.Persona
 import com.ssak3.timeattack.task.repository.entity.TaskEntity
@@ -12,7 +14,7 @@ class Task(
     val dueDatetime: LocalDateTime,
     val triggerAction: String? = null,
     val estimatedTime: Int? = null,
-    val status: TaskStatus,
+    var status: TaskStatus,
     val member: Member,
     val persona: Persona,
     val createdAt: LocalDateTime? = null,
@@ -30,6 +32,20 @@ class Task(
             member = member.toEntity(),
             persona = persona.toEntity(),
         )
+
+    /**
+     * Task의 상태를 변경한다.
+     */
+    fun changeStatus(newStatus: TaskStatus) {
+        if (this.status == newStatus) return
+
+        // 현재 상태에서 전환 가능한 상태인지 확인
+        if (!this.status.canTransitionTo(newStatus)) {
+            throw ApplicationException(ApplicationExceptionType.TASK_INVALID_STATE_TRANSITION, this.status, newStatus)
+        }
+
+        this.status = newStatus
+    }
 
     companion object {
         fun fromEntity(entity: TaskEntity) =
