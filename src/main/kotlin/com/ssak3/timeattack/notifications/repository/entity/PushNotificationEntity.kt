d2f103cfddd34f1a355050fc300e29b5df97e099
keongmini <kmnam09@gmail.com>
2025-02-28 17:24:49 +0900

    feat: save notifications

--- src/main/kotlin/com/ssak3/timeattack/notifications/repository/entity/PushNotificationEntity.kt
+++ src/main/kotlin/com/ssak3/timeattack/notifications/repository/entity/PushNotificationEntity.kt
@@ -1,10 +1,17 @@
 package com.ssak3.timeattack.notifications.repository.entity
 
 import com.ssak3.timeattack.common.domain.BaseEntity
+import com.ssak3.timeattack.member.repository.entity.MemberEntity
+import com.ssak3.timeattack.task.repository.entity.TaskEntity
 import jakarta.persistence.Column
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import java.time.LocalDateTime
 
@@ -12,13 +19,18 @@ import java.time.LocalDateTime
 @Table(name = "push_notifications")
 class PushNotificationEntity(
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    @Column(name = "member_id")
-    val memberId: Long,
-    @Column(name = "task_id")
-    val taskId: Long,
+    @ManyToOne
+    @JoinColumn(name = "member_id", foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    val member: MemberEntity,
+    @ManyToOne
+    @JoinColumn(name = "task_id", foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    val task: TaskEntity,
     @Column(name = "scheduled_at")
     val scheduledAt: LocalDateTime,
-    val status: Boolean,
+    @Column(name = "is_deleted")
+    val isDeleted: Boolean,
+    @Column(name = "order")
+    val order: Int,
 ) : BaseEntity()
