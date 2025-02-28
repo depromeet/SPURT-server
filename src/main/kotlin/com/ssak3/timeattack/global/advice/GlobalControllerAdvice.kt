162dd01b1dcc125dd2aefa435243fac7d84efc59
skydreamer <skydreamer210@gmail.com>
2025-02-28 22:50:08 +0900

    feat: advice에 exception 로깅 추가

--- src/main/kotlin/com/ssak3/timeattack/global/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/ssak3/timeattack/global/advice/GlobalControllerAdvice.kt
@@ -1,5 +1,6 @@
 package com.ssak3.timeattack.global.advice
 
+import com.ssak3.timeattack.common.utils.Logger
 import com.ssak3.timeattack.global.exception.ApplicationException
 import com.ssak3.timeattack.global.exception.ApplicationExceptionType
 import com.ssak3.timeattack.global.response.ExceptionResponse
@@ -8,14 +9,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class GlobalControllerAdvice {
+class GlobalControllerAdvice : Logger {
     @ExceptionHandler(ApplicationException::class)
     fun handleApplicationException(exception: ApplicationException): ResponseEntity<ExceptionResponse> {
+        logger.error("ApplicationException occurred", exception)
         return ResponseEntity.status(exception.exceptionType.httpStatus).body(ExceptionResponse.from(exception))
     }
 
     @ExceptionHandler(Exception::class)
     fun handleException(exception: Exception): ResponseEntity<ExceptionResponse> {
+        logger.error("Exception occurred", exception)
         val exceptionType = ApplicationExceptionType.UNDEFINED_EXCEPTION
         return ResponseEntity.status(
             exceptionType.httpStatus,
