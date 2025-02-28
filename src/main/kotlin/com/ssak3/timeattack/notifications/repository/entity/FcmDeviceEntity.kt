33d7b049006612fb163923df7321dc86d0be142c
keongmini <kmnam09@gmail.com>
2025-02-28 17:26:00 +0900

    feat: save device

---
+++ src/main/kotlin/com/ssak3/timeattack/notifications/repository/entity/FcmDeviceEntity.kt
@@ -0,0 +1,35 @@
+package com.ssak3.timeattack.notifications.repository.entity
+
+import com.ssak3.timeattack.common.domain.BaseEntity
+import com.ssak3.timeattack.common.domain.DevicePlatform
+import com.ssak3.timeattack.member.repository.entity.MemberEntity
+import jakarta.persistence.Column
+import jakarta.persistence.ConstraintMode
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "fcm_devices")
+class FcmDeviceEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    @ManyToOne
+    @JoinColumn(name = "member_id", foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    val member: MemberEntity,
+    @Column(name = "fcm_registration_token", length = 500)
+    val fcmRegistrationToken: String,
+    @Column(name = "device_platform", length = 20)
+    @Enumerated(value = EnumType.STRING)
+    val devicePlatform: DevicePlatform,
+    @Column(name = "status", columnDefinition = "TINYINT")
+    val status: Boolean,
+) : BaseEntity()
