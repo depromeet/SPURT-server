313b9e4f895813a088cea60c4d4397d38763a7c6
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-03-02 16:53:57 +0900

    이탈한 작업 조회 (#100)
    - #88 
    - #90 
    - #91 
    
    ### Proposed Changes
    - 이탈한 작업 1개 조회하는 메서드 개발
    
    ### Code Review Point
    - 테스트 코드가 조건을 잘 커버해서 테스트하는지

--- src/main/kotlin/com/ssak3/timeattack/task/domain/TaskStatus.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/domain/TaskStatus.kt
@@ -4,6 +4,7 @@ enum class TaskStatus {
     BEFORE,
     WARMING_UP,
     PROCRASTINATING,
+    HOLDING_OFF,
     FOCUSED,
     COMPLETE,
     FAIL,
@@ -13,9 +14,11 @@ enum class TaskStatus {
         return when (this) {
             // 항상 다음 상태로만 전환 가능
             BEFORE ->
-                next == WARMING_UP || next == PROCRASTINATING || next == FOCUSED || next == COMPLETE || next == FAIL
-            WARMING_UP -> next == FOCUSED || next == COMPLETE || next == FAIL
+                next == WARMING_UP || next == HOLDING_OFF || next == FOCUSED || next == FAIL
+            WARMING_UP -> next == FOCUSED || next == FAIL
+            HOLDING_OFF -> next == FOCUSED || next == PROCRASTINATING || next == FAIL
             PROCRASTINATING -> next == FOCUSED || next == FAIL
+            // TODO: FOCUSED에서 마감시간 지났을 때 FAIL로 처리할 지 고민
             FOCUSED -> next == COMPLETE
             // 최종 상태에서는 더 이상 전환 불가
             COMPLETE, FAIL -> false
