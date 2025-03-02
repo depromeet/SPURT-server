f0094fc52c2c0235ab486a6df7dab6ad902f6e38
skydreamer <skydreamer210@gmail.com>
2025-03-02 18:40:23 +0900

    fix: 이벤트 리스너 필요한 부분에 TransactionalEventListener(AFTER_COMMIT) 적용

--- src/main/kotlin/com/ssak3/timeattack/notifications/service/PushNotificationListener.kt
+++ src/main/kotlin/com/ssak3/timeattack/notifications/service/PushNotificationListener.kt
@@ -1,5 +1,7 @@
 package com.ssak3.timeattack.notifications.service
 
+import com.google.cloud.firestore.telemetry.MetricsUtil.logger
+import com.ssak3.timeattack.common.utils.Logger
 import com.ssak3.timeattack.member.service.MemberService
 import com.ssak3.timeattack.notifications.domain.PushNotification
 import com.ssak3.timeattack.task.service.TaskService
@@ -8,14 +10,16 @@ import com.ssak3.timeattack.task.service.events.ReminderSaveEvent
 import com.ssak3.timeattack.task.service.events.ScheduledTaskSaveEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class PushNotificationListener(
     private val pushNotificationService: PushNotificationService,
     private val memberService: MemberService,
     private val taskService: TaskService,
-) {
-    @EventListener
+) : Logger {
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun saveNotification(event: ScheduledTaskSaveEvent) {
         val member = memberService.getMemberById(event.memberId)
         val task = taskService.getTaskById(event.taskId)
@@ -49,8 +53,9 @@ class PushNotificationListener(
         pushNotificationService.saveAll(pushNotifications)
     }
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun deleteNotifications(event: DeleteTaskEvent) {
+//        logger.info("Delete notifications by task id: ${event.taskId}")
         TODO("알림 삭제 이벤트를 받아 db에서 비활성화 처리")
     }
 }
