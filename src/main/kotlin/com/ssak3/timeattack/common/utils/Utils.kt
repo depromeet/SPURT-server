ccff50a939ff737d8b54e21c5a532e1da2b94a4d
skydreamer <skydreamer210@gmail.com>
2025-03-07 20:52:42 +0900

    comment: 주석 수정

--- src/main/kotlin/com/ssak3/timeattack/common/utils/Utils.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/utils/Utils.kt
@@ -2,7 +2,7 @@ package com.ssak3.timeattack.common.utils
 
 /**
  * value가 null이 아닌지 체크합니다.
- * null 이라면 "${attributeName} must not be null" 이라는 예외 메시지를 가진 IllegalArgumentException을 발생시킵니다.
+ * null 이라면 "${valueName} must not be null" 이라는 예외 메시지를 가진 IllegalArgumentException을 발생시킵니다.
  * @param value 체크할 값
  * @param valueName 예외 메시지에 사용할 속성 이름
  */
