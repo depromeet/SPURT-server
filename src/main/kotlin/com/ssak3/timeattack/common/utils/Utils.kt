e00190666ae2782a00b3c87f49235c5c254f5a50
skydreamer <skydreamer210@gmail.com>
2025-03-07 15:26:58 +0900

    refactor: 파라미터명 수정

--- src/main/kotlin/com/ssak3/timeattack/common/utils/Utils.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/utils/Utils.kt
@@ -7,21 +7,21 @@ import kotlin.contracts.contract
  * value가 null이 아닌지 체크합니다.
  * null 이라면 "${attributeName} must not be null" 이라는 예외 메시지를 가진 IllegalArgumentException을 발생시킵니다.
  * @param value 체크할 값
- * @param attributeName 예외 메시지에 사용할 속성 이름
+ * @param valueName 예외 메시지에 사용할 속성 이름
  */
 @OptIn(ExperimentalContracts::class)
 fun <T : Any> checkNotNull(
     value: T?,
-    attributeName: String?,
+    valueName: String?,
 ): T {
     contract {
         returns() implies (value != null)
     }
     if (value == null) {
-        if (attributeName == null) {
+        if (valueName == null) {
             throw IllegalArgumentException("Required value must not be null")
         }
-        throw IllegalArgumentException("$attributeName must not be null")
+        throw IllegalArgumentException("$valueName must not be null")
     }
     return value
 }
