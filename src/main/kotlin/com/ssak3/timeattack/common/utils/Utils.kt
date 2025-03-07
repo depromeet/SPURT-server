d442f27a42e70e6feb447d627e68eebf92b31d52
skydreamer <skydreamer210@gmail.com>
2025-03-07 20:49:21 +0900

    refactor: checkNotNull 리팩토링

--- src/main/kotlin/com/ssak3/timeattack/common/utils/Utils.kt
+++ src/main/kotlin/com/ssak3/timeattack/common/utils/Utils.kt
@@ -1,8 +1,5 @@
 package com.ssak3.timeattack.common.utils
 
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.contract
-
 /**
  * value가 null이 아닌지 체크합니다.
  * null 이라면 "${attributeName} must not be null" 이라는 예외 메시지를 가진 IllegalArgumentException을 발생시킵니다.
@@ -13,5 +10,6 @@ fun <T : Any> checkNotNull(
     value: T?,
     valueName: String?,
 ): T {
-    return checkNotNull(value) { "$valueName must not be null" }
+    val message = valueName?.let { "$it must not be null" } ?: "Value must not be null"
+    return checkNotNull(value) { message }
 }
