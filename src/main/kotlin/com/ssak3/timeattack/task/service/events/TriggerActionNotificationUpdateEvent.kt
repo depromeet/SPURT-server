7f36e90b40781065f957012a6441aaab181009d7
skydreamer <skydreamer210@gmail.com>
2025-03-08 17:48:00 +0900

    refactor: ktlint format

--- src/main/kotlin/com/ssak3/timeattack/task/service/events/TriggerActionNotificationUpdateEvent.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/service/events/TriggerActionNotificationUpdateEvent.kt
@@ -2,8 +2,8 @@ package com.ssak3.timeattack.task.service.events
 
 import java.time.LocalDateTime
 
-data class TriggerActionNotificationUpdateEvent (
+data class TriggerActionNotificationUpdateEvent(
     val memberId: Long,
     val taskId: Long,
-    val alarmTime: LocalDateTime
+    val alarmTime: LocalDateTime,
 )
