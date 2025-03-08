74e823da4561847993d2c0aefb4206fc52517c1a
skydreamer <skydreamer210@gmail.com>
2025-03-09 02:13:10 +0900

    test: TaskStatusTest Kotest로 수정

--- src/test/kotlin/com/ssak3/timeattack/task/domain/TaskStatusTest.kt
+++ src/test/kotlin/com/ssak3/timeattack/task/domain/TaskStatusTest.kt
@@ -1,84 +1,77 @@
 package com.ssak3.timeattack.task.domain
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import com.ssak3.timeattack.task.domain.TaskStatus.BEFORE
+import com.ssak3.timeattack.task.domain.TaskStatus.COMPLETE
+import com.ssak3.timeattack.task.domain.TaskStatus.FAIL
+import com.ssak3.timeattack.task.domain.TaskStatus.FOCUSED
+import com.ssak3.timeattack.task.domain.TaskStatus.HOLDING_OFF
+import com.ssak3.timeattack.task.domain.TaskStatus.PROCRASTINATING
+import com.ssak3.timeattack.task.domain.TaskStatus.WARMING_UP
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
-class TaskStatusTest {
-    @ParameterizedTest
-    @DisplayName("canTransitionTo 메서드는 현재 상태에서 이후 상태로의 전환을 허용한다")
-    @CsvSource(
-        value = [
-            "BEFORE, WARMING_UP",
-            "BEFORE, HOLDING_OFF",
-            "BEFORE, FOCUSED",
-            "BEFORE, FAIL",
-            "WARMING_UP, FOCUSED",
-            "WARMING_UP, FAIL",
-            "HOLDING_OFF, FOCUSED",
-            "HOLDING_OFF, PROCRASTINATING",
-            "HOLDING_OFF, FAIL",
-            "PROCRASTINATING, FOCUSED",
-            "PROCRASTINATING, FAIL",
-            "FOCUSED, COMPLETE",
-        ],
-    )
-    fun allowValidTransition(
-        initialStatus: TaskStatus,
-        newStatus: TaskStatus,
-    ) {
-        // when
-        val result = initialStatus.canTransitionTo(newStatus)
+class TaskStatusTest : DescribeSpec({
+    describe("canTransitionTo 메서드는") {
+        context("현재 상태에서 이후 상태로의 전환을 허용한다") {
+            forAll(
+                row(BEFORE, WARMING_UP),
+                row(BEFORE, HOLDING_OFF),
+                row(BEFORE, FOCUSED),
+                row(BEFORE, FAIL),
+                row(WARMING_UP, FOCUSED),
+                row(WARMING_UP, FAIL),
+                row(HOLDING_OFF, FOCUSED),
+                row(HOLDING_OFF, PROCRASTINATING),
+                row(HOLDING_OFF, FAIL),
+                row(PROCRASTINATING, FOCUSED),
+                row(PROCRASTINATING, FAIL),
+                row(FOCUSED, COMPLETE),
+            ) { initialStatus, newStatus ->
+                it("$initialStatus -> $newStatus 전환은 허용된다") {
+                    initialStatus.canTransitionTo(newStatus) shouldBe true
+                }
+            }
+        }
 
-        // then
-        assertThat(result).isTrue()
+        context("유효하지 않은 상태 전환 시 false를 반환한다") {
+            forAll(
+                row(COMPLETE, BEFORE),
+                row(COMPLETE, WARMING_UP),
+                row(COMPLETE, PROCRASTINATING),
+                row(COMPLETE, HOLDING_OFF),
+                row(COMPLETE, FOCUSED),
+                row(COMPLETE, FAIL),
+                row(FAIL, BEFORE),
+                row(FAIL, WARMING_UP),
+                row(FAIL, PROCRASTINATING),
+                row(FAIL, HOLDING_OFF),
+                row(FAIL, FOCUSED),
+                row(FAIL, COMPLETE),
+                row(FOCUSED, BEFORE),
+                row(FOCUSED, WARMING_UP),
+                row(FOCUSED, PROCRASTINATING),
+                row(FOCUSED, HOLDING_OFF),
+                row(FOCUSED, FAIL),
+                row(PROCRASTINATING, BEFORE),
+                row(PROCRASTINATING, WARMING_UP),
+                row(PROCRASTINATING, HOLDING_OFF),
+                row(PROCRASTINATING, COMPLETE),
+                row(HOLDING_OFF, BEFORE),
+                row(HOLDING_OFF, WARMING_UP),
+                row(HOLDING_OFF, COMPLETE),
+                row(WARMING_UP, BEFORE),
+                row(WARMING_UP, HOLDING_OFF),
+                row(WARMING_UP, PROCRASTINATING),
+                row(WARMING_UP, COMPLETE),
+                row(BEFORE, PROCRASTINATING),
+                row(BEFORE, COMPLETE),
+            ) { initialStatus, newStatus ->
+                it("$initialStatus -> $newStatus 전환은 허용되지 않는다") {
+                    initialStatus.canTransitionTo(newStatus) shouldBe false
+                }
+            }
+        }
     }
-
-    @ParameterizedTest
-    @DisplayName("canTransitionTo 메서드는 유효하지 않은 상태 전환 시 false를 반환한다")
-    @CsvSource(
-        value = [
-            "COMPLETE, BEFORE",
-            "COMPLETE, WARMING_UP",
-            "COMPLETE, PROCRASTINATING",
-            "COMPLETE, HOLDING_OFF",
-            "COMPLETE, FOCUSED",
-            "COMPLETE, FAIL",
-            "FAIL, BEFORE",
-            "FAIL, WARMING_UP",
-            "FAIL, PROCRASTINATING",
-            "FAIL, HOLDING_OFF",
-            "FAIL, FOCUSED",
-            "FAIL, COMPLETE",
-            "FOCUSED, BEFORE",
-            "FOCUSED, WARMING_UP",
-            "FOCUSED, PROCRASTINATING",
-            "FOCUSED, HOLDING_OFF",
-            "FOCUSED, FAIL",
-            "PROCRASTINATING, BEFORE",
-            "PROCRASTINATING, WARMING_UP",
-            "PROCRASTINATING, HOLDING_OFF",
-            "PROCRASTINATING, COMPLETE",
-            "HOLDING_OFF, BEFORE",
-            "HOLDING_OFF, WARMING_UP",
-            "HOLDING_OFF, COMPLETE",
-            "WARMING_UP, BEFORE",
-            "WARMING_UP, HOLDING_OFF",
-            "WARMING_UP, PROCRASTINATING",
-            "WARMING_UP, COMPLETE",
-            "BEFORE, PROCRASTINATING",
-            "BEFORE, COMPLETE",
-        ],
-    )
-    fun throwExceptionForInvalidTransition(
-        initialStatus: TaskStatus,
-        newStatus: TaskStatus,
-    ) {
-        // when
-        val result = initialStatus.canTransitionTo(newStatus)
-
-        // then
-        assertThat(result).isFalse()
-    }
-}
+})
