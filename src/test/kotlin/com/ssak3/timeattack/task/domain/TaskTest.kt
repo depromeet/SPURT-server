5d0bc541ccc231d53d2cb48f25ade7b8b05b81df
skydreamer <skydreamer210@gmail.com>
2025-03-09 21:06:43 +0900

    test: 작업 소요시간 변경 상태 조건 도메인 단위 테스트 작성

--- src/test/kotlin/com/ssak3/timeattack/task/domain/TaskTest.kt
+++ src/test/kotlin/com/ssak3/timeattack/task/domain/TaskTest.kt
@@ -1,5 +1,7 @@
 package com.ssak3.timeattack.task.domain
 
+import com.ssak3.timeattack.common.exception.ApplicationException
+import com.ssak3.timeattack.common.exception.ApplicationExceptionType
 import com.ssak3.timeattack.fixture.Fixture
 import com.ssak3.timeattack.task.domain.TaskStatus.BEFORE
 import com.ssak3.timeattack.task.domain.TaskStatus.COMPLETE
@@ -8,20 +10,24 @@ import com.ssak3.timeattack.task.domain.TaskStatus.FOCUSED
 import com.ssak3.timeattack.task.domain.TaskStatus.HOLDING_OFF
 import com.ssak3.timeattack.task.domain.TaskStatus.PROCRASTINATING
 import com.ssak3.timeattack.task.domain.TaskStatus.WARMING_UP
+import com.ssak3.timeattack.utils.listToRowArray
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.data.forAll
-import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+import java.time.LocalDateTime
 
 class TaskTest : DescribeSpec({
     describe("Task 클래스 수정 시") {
         context("Urgent Task의 모든 가능한 Status에서") {
-            forAll(
-                row(FOCUSED),
-                row(FAIL),
-                row(COMPLETE),
-            ) { status ->
-                it("이름 수정이 가능하다. (status : $status)") {
+            val statusList = listOf(
+                FOCUSED,
+                FAIL,
+                COMPLETE,
+            )
+            it("이름 수정이 가능하다.") {
+                forAll(*listToRowArray(statusList)) { status ->
                     val task =
                         Fixture.createUrgentTask(
                             status = status,
@@ -35,16 +41,17 @@ class TaskTest : DescribeSpec({
         }
 
         context("Scheduled Task의 모든 가능한 Status에서") {
-            forAll(
-                row(BEFORE),
-                row(WARMING_UP),
-                row(HOLDING_OFF),
-                row(FOCUSED),
-                row(FAIL),
-                row(PROCRASTINATING),
-                row(COMPLETE),
-            ) { status ->
-                it("이름 수정이 가능하다. (status : $status)") {
+            val statusList = listOf(
+                BEFORE,
+                WARMING_UP,
+                HOLDING_OFF,
+                FOCUSED,
+                FAIL,
+                PROCRASTINATING,
+                COMPLETE,
+            )
+            it("이름 수정이 가능하다.") {
+                forAll(*listToRowArray(statusList)) { status ->
                     val task =
                         Fixture.createScheduledTask(
                             status = status,
@@ -56,5 +63,68 @@ class TaskTest : DescribeSpec({
                 }
             }
         }
+
+
+        context("BEFORE 상태에서") {
+            lateinit var task: Task
+            lateinit var now: LocalDateTime
+
+            beforeEach {
+                now = LocalDateTime.now()
+                task = Fixture.createScheduledTask(
+                    status = BEFORE,
+                    dueDatetime = now.plusHours(2),
+                    estimatedTime = 60,
+                    triggerActionAlarmTime = now.plusMinutes(30)
+                )
+            }
+
+            it("작은 행동 수정이 가능하다.") {
+                shouldNotThrowAny {
+                    task.modifyTriggerAction("modified trigger action")
+                }
+            }
+
+            it ("유효한 예상 소요 시간 수정이 가능하다.") {
+                shouldNotThrowAny {
+                    task.modifyEstimatedTime(10, now.plusMinutes(20))
+                }
+            }
+        }
+
+        context("BEFORE가 아닌 상태에서") {
+            val statusExceptBefore = listOf(
+                WARMING_UP,
+                HOLDING_OFF,
+                FOCUSED,
+                FAIL,
+                PROCRASTINATING,
+                COMPLETE
+            )
+
+            it("작은 행동 수정이 불가능하다.") {
+                forAll(*listToRowArray(statusExceptBefore)) { status ->
+                    val task = Fixture.createScheduledTask(status = status)
+                    shouldThrow<ApplicationException> {
+                        task.modifyTriggerAction("modified trigger action")
+                    }.apply {
+                        this.exceptionType shouldBe ApplicationExceptionType.INVALID_TASK_STATUS_FOR_UPDATE
+                    }
+                }
+            }
+
+            it("유효한 예상 소요 시간이어도 수정이 불가능하다.") {
+                forAll(*listToRowArray(statusExceptBefore)) { status ->
+                    val task = Fixture.createScheduledTask(status = status)
+                    shouldThrow<ApplicationException> {
+                        task.modifyEstimatedTime(10, LocalDateTime.now().plusMinutes(20))
+                    }.apply {
+                        this.exceptionType shouldBe ApplicationExceptionType.INVALID_TASK_STATUS_FOR_UPDATE
+                    }
+                }
+            }
+        }
+
+
     }
 })
