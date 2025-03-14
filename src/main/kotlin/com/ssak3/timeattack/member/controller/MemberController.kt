8830eb440473769392b90c299020b2671a6ab9e0
nick <123347183+CEO-Nick@users.noreply.github.com>
2025-03-14 12:22:44 +0900

    현재 유저 정보 조회 API 개발 (#148)
    #147 
    
    ### Proposed Changes
    - accessToken에 해당하는 유저 정보 반환하는 API 개발
    
    ### Code Review Point

---
+++ src/main/kotlin/com/ssak3/timeattack/member/controller/MemberController.kt
@@ -0,0 +1,33 @@
+package com.ssak3.timeattack.member.controller
+
+import com.ssak3.timeattack.common.config.SwaggerConfig.Companion.SECURITY_SCHEME_NAME
+import com.ssak3.timeattack.common.utils.checkNotNull
+import com.ssak3.timeattack.member.controller.dto.MemberInfoResponse
+import com.ssak3.timeattack.member.domain.Member
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/members")
+class MemberController {
+    @Operation(summary = "현재 유저 정보 조회", security = [SecurityRequirement(name = SECURITY_SCHEME_NAME)])
+    @GetMapping("/me")
+    fun getCurrentUser(
+        @AuthenticationPrincipal member: Member,
+    ): ResponseEntity<MemberInfoResponse> {
+        checkNotNull(member.id, "memberId")
+        return ResponseEntity.ok(
+            MemberInfoResponse(
+                memberId = member.id,
+                nickname = member.nickname,
+                email = member.email,
+                profileImageUrl = member.profileImageUrl,
+            ),
+        )
+    }
+}
