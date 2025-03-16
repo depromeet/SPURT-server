676733b7aace7e228938c31dcbd967b4c6b37d7d
keongmini <kmnam09@gmail.com>
2025-03-17 03:01:45 +0900

    feat: lint 수정

--- src/main/kotlin/com/ssak3/timeattack/task/controller/SubtaskController.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/controller/SubtaskController.kt
@@ -12,7 +12,13 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Positive
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/subtasks")
