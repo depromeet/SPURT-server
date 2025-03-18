59f38f676712c3682f34704a873859ca7fbf7570
keongmini <kmnam09@gmail.com>
2025-03-18 22:12:05 +0900

    feat: 리뷰 반영

--- src/main/kotlin/com/ssak3/timeattack/task/controller/dto/SubTaskResponse.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/controller/dto/SubTaskResponse.kt
@@ -7,7 +7,6 @@ data class SubTaskResponse(
     val id: Long,
     val taskId: Long,
     val name: String,
-    val isDeleted: Boolean,
     val isCompleted: Boolean,
 ) {
     companion object {
@@ -16,7 +15,6 @@ data class SubTaskResponse(
                 id = subtask.id,
                 taskId = checkNotNull(subtask.task.id, "task id"),
                 name = subtask.name,
-                isDeleted = subtask.isDeleted,
                 isCompleted = subtask.isCompleted,
             )
     }
