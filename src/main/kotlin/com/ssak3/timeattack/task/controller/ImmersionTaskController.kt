245f0503ee7358a9b7a4aa14e7de406a88c29514
keongmini <kmnam09@gmail.com>
2025-03-18 23:17:26 +0900

    feat: modify controller path

--- src/main/kotlin/com/ssak3/timeattack/task/controller/ImmersionTaskController.kt
+++ src/main/kotlin/com/ssak3/timeattack/task/controller/ImmersionTaskController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/immersion")
+@RequestMapping("/v1/immersion-tasks")
 class ImmersionTaskController(
     private val immersionTaskService: ImmersionTaskService,
 ) {
