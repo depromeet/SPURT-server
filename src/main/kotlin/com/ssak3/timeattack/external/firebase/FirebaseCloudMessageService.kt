f35cf9479ce79acf8b09481cb23bc3a71608e3c8
skydreamer <skydreamer210@gmail.com>
2025-03-24 00:28:30 +0900

    fix : fcm send 중 에러 발생시 에러 처리 로직 추가
    에러를 catch 하면 해당 에러 때문에 send 가 중지되지 않고 가능한 알림을 다 보내기 위해

--- src/main/kotlin/com/ssak3/timeattack/external/firebase/FirebaseCloudMessageService.kt
+++ src/main/kotlin/com/ssak3/timeattack/external/firebase/FirebaseCloudMessageService.kt
@@ -1,6 +1,7 @@
 package com.ssak3.timeattack.external.firebase
 
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingException
 import com.google.firebase.messaging.Message
 import com.google.firebase.messaging.Notification
 import com.ssak3.timeattack.common.utils.Logger
@@ -26,9 +27,14 @@ class FirebaseCloudMessageService : Logger {
                 )
                 .build()
 
-        val response = firebaseMessaging.send(fcmMessage)
-
-        logger.info("[Firebase Messaging] send successfully: $response")
+        var response = ""
+        try {
+            response = firebaseMessaging.send(fcmMessage)
+        } catch (exception: FirebaseMessagingException) {
+            logger.error("[${exception.errorCode}] Error sending message: ${exception.message}", exception)
+            // TODO: 잘못된 토큰 에러 발생 시 토큰 삭제 로직 추가
+            // TODO: 토큰 이외에 실패시 메시지 큐등을 활용해 재시도 로직 추가
+        }
 
         return response
     }
