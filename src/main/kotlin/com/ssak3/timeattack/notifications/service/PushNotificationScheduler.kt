14156f159c23e384fb4c4d74588e82110256ec59
skydreamer <skydreamer210@gmail.com>
2025-03-23 20:54:18 +0900

    fix: PushNotificationScheduler 로그 추가 및 프록시 설정

--- src/main/kotlin/com/ssak3/timeattack/notifications/service/PushNotificationScheduler.kt
+++ src/main/kotlin/com/ssak3/timeattack/notifications/service/PushNotificationScheduler.kt
@@ -1,10 +1,12 @@
 package com.ssak3.timeattack.notifications.service
 
+import com.ssak3.timeattack.common.utils.Logger
 import com.ssak3.timeattack.external.firebase.domain.DevicePlatform
 import com.ssak3.timeattack.notifications.domain.FcmMessage
 import com.ssak3.timeattack.notifications.domain.FcmNotificationConstants.getMessage
 import com.ssak3.timeattack.notifications.domain.FcmNotificationConstants.getRoute
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
@@ -12,16 +14,18 @@ import org.springframework.stereotype.Component
 
 @Component
 @EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ConditionalOnProperty(name = ["fcm.scheduler.status"], havingValue = "true")
 class PushNotificationScheduler(
     private val pushNotificationService: PushNotificationService,
     private val fcmDeviceService: FcmDeviceService,
     private val fcmPushNotificationService: FcmPushNotificationService,
-) {
+) : Logger {
     @Async
     @Scheduled(cron = "0 0/5 * * * ?") // 5분 단위
     fun sendNotifications() {
         val currentScheduledNotifications = pushNotificationService.getNotificationsByCurrentTime()
+        logger.info("[Push Notification Scheduler] currentScheduledNotifications: $currentScheduledNotifications")
         currentScheduledNotifications.forEach {
             checkNotNull(it.member.id)
             checkNotNull(it.task.id)
