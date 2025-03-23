1930df96668637ccba7c7a9dca2b3fb01f715482
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2025-03-23 20:06:50 +0900

    Jpa 로그 제거 및 알림 로그 추가 Main merge (#168)
    (# 입력 하고 이슈 선택)
    
    ### Proposed Changes
    (PR 목적과 변경사항을 적어주세요.)
    
    ### Code Review Point
    (리뷰어가 중점적으로 보면 좋을 부분을 적어주세요.)
    
    ---------
    
    Co-authored-by: nick <123347183+CEO-Nick@users.noreply.github.com>
    Co-authored-by: nick <andantej281@gmail.com>
    Co-authored-by: keongmini <kmnam09@gmail.com>

--- src/main/kotlin/com/ssak3/timeattack/notifications/service/FcmDeviceListener.kt
+++ src/main/kotlin/com/ssak3/timeattack/notifications/service/FcmDeviceListener.kt
@@ -1,5 +1,6 @@
 package com.ssak3.timeattack.notifications.service
 
+import com.ssak3.timeattack.common.utils.Logger
 import com.ssak3.timeattack.member.service.MemberService
 import com.ssak3.timeattack.member.service.events.DeviceRegisterEvent
 import com.ssak3.timeattack.notifications.domain.FcmDevice
@@ -10,9 +11,10 @@ import org.springframework.stereotype.Component
 class FcmDeviceListener(
     private val fcmDeviceService: FcmDeviceService,
     private val memberService: MemberService,
-) {
+) : Logger {
     @EventListener
     fun save(event: DeviceRegisterEvent) {
+        logger.info("DeviceRegisterEvent: $event")
         val member = memberService.getMemberById(event.memberId)
         val fcmDevice =
             FcmDevice(
