f6dd901011893e0377930ff86622f9d3b827e195
nick <andantej281@gmail.com>
2025-03-25 17:43:32 +0900

    test: 불필요한 mock 삭제 및 수정

--- src/test/kotlin/com/ssak3/timeattack/task/scheduler/TaskSchedulerInitializerTest.kt
+++ src/test/kotlin/com/ssak3/timeattack/task/scheduler/TaskSchedulerInitializerTest.kt
@@ -1,23 +1,18 @@
 package com.ssak3.timeattack.task.scheduler
 
 import com.ssak3.timeattack.fixture.Fixture
-import com.ssak3.timeattack.task.domain.Task
 import com.ssak3.timeattack.task.domain.TaskStatus
 import com.ssak3.timeattack.task.domain.TaskStatus.Companion.statusesToFail
 import com.ssak3.timeattack.task.repository.TaskRepository
-import com.ssak3.timeattack.task.repository.entity.TaskEntity
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.Runs
 import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
-import io.mockk.mockkObject
-import io.mockk.unmockkAll
 import io.mockk.verify
 import java.time.LocalDateTime
 
-// @IntegrationTest
 class TaskSchedulerInitializerTest : BehaviorSpec({
     // 테스트 대상 객체와 의존성
     val taskRepository = mockk<TaskRepository>()
@@ -51,16 +46,13 @@ class TaskSchedulerInitializerTest : BehaviorSpec({
             dueDatetime = dayAfterTomorrow,
         )
 
-    // 테스트용 TaskEntity 모킹
-    val taskEntity1 = mockk<TaskEntity>()
-    val taskEntity2 = mockk<TaskEntity>()
-    val taskEntity3 = mockk<TaskEntity>()
-
-    // Task.fromEntity 모킹
-    mockkObject(Task.Companion)
-    every { Task.fromEntity(taskEntity1) } returns task1
-    every { Task.fromEntity(taskEntity2) } returns task2
-    every { Task.fromEntity(taskEntity3) } returns task3
+    // 테스트용 TaskEntity 생성
+    val taskEntities =
+        listOf(
+            task1.toEntity(),
+            task2.toEntity(),
+            task3.toEntity(),
+        )
 
     Given("실패 처리 대상이 되는 작업들이 있을 때") {
         // 이전 테스트의 모킹 초기화
@@ -69,7 +61,7 @@ class TaskSchedulerInitializerTest : BehaviorSpec({
         // 리포지토리 모킹 설정
         every {
             taskRepository.findTodoTasks(statusesToFail)
-        } returns listOf(taskEntity1, taskEntity2, taskEntity3)
+        } returns taskEntities
 
         // 스케줄러 모킹 설정
         every {
@@ -80,16 +72,16 @@ class TaskSchedulerInitializerTest : BehaviorSpec({
             taskSchedulerInitializer.initializeTaskSchedulers()
 
             Then("모든 대상 작업에 대해 스케줄러가 등록되어야 한다") {
-                // 메서드 호출 횟수 검증
-                verify(exactly = 1) {
-                    taskRepository.findTodoTasks(statusesToFail)
+                // 등록된 작업 수 검증
+                verify(exactly = 3) {
+                    overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(any())
                 }
 
-                // 각 작업에 대해 스케줄러 등록 검증
-                verify(exactly = 1) {
-                    overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(task1)
-                    overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(task2)
-                    overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(task3)
+                // ID로 작업 검증
+                verify {
+                    overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(match { it.id == 1L })
+                    overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(match { it.id == 2L })
+                    overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(match { it.id == 3L })
                 }
             }
         }
@@ -108,21 +100,11 @@ class TaskSchedulerInitializerTest : BehaviorSpec({
             taskSchedulerInitializer.initializeTaskSchedulers()
 
             Then("스케줄러 등록이 호출되지 않아야 한다") {
-                // 메서드 호출 검증
-                verify(exactly = 1) {
-                    taskRepository.findTodoTasks(statusesToFail)
-                }
-
                 // 스케줄러 등록 호출 안 됨 검증
                 verify(exactly = 0) {
                     overdueTaskFailureScheduler.scheduleTaskTimeoutFailure(any())
                 }
             }
         }
-    }
-
-    // 테스트 종료 후 모킹 해제
-    afterSpec {
-        unmockkAll()
     }
 })
