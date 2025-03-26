661d0dd8d876ba1b20fae0f56d313a7f6518403f
nick <andantej281@gmail.com>
2025-03-27 00:16:09 +0900

    feat: createdAt, updatedAt 추가

--- src/main/kotlin/com/ssak3/timeattack/retrospection/domain/Retrospection.kt
+++ src/main/kotlin/com/ssak3/timeattack/retrospection/domain/Retrospection.kt
@@ -3,6 +3,7 @@ package com.ssak3.timeattack.retrospection.domain
 import com.ssak3.timeattack.member.domain.Member
 import com.ssak3.timeattack.retrospection.repository.entity.RetrospectionEntity
 import com.ssak3.timeattack.task.domain.Task
+import java.time.LocalDateTime
 
 class Retrospection(
     val id: Long? = null,
@@ -11,6 +12,8 @@ class Retrospection(
     val satisfaction: Int,
     val concentration: Int,
     val comment: String? = null,
+    val createdAt: LocalDateTime? = null,
+    val updatedAt: LocalDateTime? = null,
 ) {
     fun toEntity() =
         RetrospectionEntity(
@@ -31,6 +34,8 @@ class Retrospection(
                 satisfaction = entity.satisfaction,
                 concentration = entity.concentration,
                 comment = entity.comment,
+                createdAt = entity.createdAt,
+                updatedAt = entity.updatedAt,
             )
     }
 }
