09907fb055620bd5e1e4c9d4604e545687ddaa92
keongmini <kmnam09@gmail.com>
2025-04-03 00:28:27 +0900

    feat: remove

--- src/main/kotlin/com/ssak3/timeattack/task/controller/dto/ImmersionResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.ssak3.timeattack.task.controller.dto
-
-import com.ssak3.timeattack.task.domain.ImmersionTask
-
-data class ImmersionResponse(
-    val immersionTasks: List<ImmersionTask>,
-)
