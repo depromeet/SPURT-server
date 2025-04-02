9a60b31b02d2a30292d64a7d8b414adf8f60246b
keongmini <kmnam09@gmail.com>
2025-04-03 00:39:14 +0900

    feat: remove youtube client (#200)
    (# 입력 하고 이슈 선택)
    
    ### Proposed Changes
    (PR 목적과 변경사항을 적어주세요.)
    
    ### Code Review Point
    (리뷰어가 중점적으로 보면 좋을 부분을 적어주세요.)

--- src/main/kotlin/com/ssak3/timeattack/persona/client/GoogleCloudProperties.kt
+++
@@ -1,8 +0,0 @@
-package com.ssak3.timeattack.persona.client
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "google.cloud")
-data class GoogleCloudProperties(
-    val apiKey: String,
-)
--- src/main/kotlin/com/ssak3/timeattack/persona/controller/dto/PersonaPlaylistResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.ssak3.timeattack.persona.controller.dto
-
-data class PersonaPlaylistResponse(
-    val playlistIds: List<String>,
-)
