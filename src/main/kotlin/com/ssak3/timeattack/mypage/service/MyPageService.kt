26507eeb42ae61eead2d808187a772bdf5b78df4
nick <andantej281@gmail.com>
2025-04-03 14:10:45 +0900

    refactor: 리뷰 반영 - MyPageService에서 Repository 레이어에 의존하도록 수정

--- src/main/kotlin/com/ssak3/timeattack/mypage/service/MyPageService.kt
+++ src/main/kotlin/com/ssak3/timeattack/mypage/service/MyPageService.kt
@@ -3,43 +3,51 @@ package com.ssak3.timeattack.mypage.service
 import com.ssak3.timeattack.common.utils.Logger
 import com.ssak3.timeattack.common.utils.checkNotNull
 import com.ssak3.timeattack.member.domain.Member
-import com.ssak3.timeattack.mypage.controller.dto.MyPageResponse
-import com.ssak3.timeattack.persona.service.PersonaService
-import com.ssak3.timeattack.persona.service.dto.PersonaDto
-import com.ssak3.timeattack.retrospection.service.RetrospectionService
-import com.ssak3.timeattack.task.controller.dto.TaskResponse
-import com.ssak3.timeattack.task.service.TaskService
+import com.ssak3.timeattack.mypage.service.dto.MyPageDto
+import com.ssak3.timeattack.persona.domain.Persona
+import com.ssak3.timeattack.persona.repository.PersonaRepository
+import com.ssak3.timeattack.retrospection.repository.RetrospectionRepository
+import com.ssak3.timeattack.task.domain.Task
+import com.ssak3.timeattack.task.repository.TaskRepository
 import org.springframework.stereotype.Service
 
 @Service
 class MyPageService(
-    private val taskService: TaskService,
-    private val personaService: PersonaService,
-    private val retrospectionService: RetrospectionService,
+    private val taskRepository: TaskRepository,
+    private val personaRepository: PersonaRepository,
+    private val retrospectionRepository: RetrospectionRepository,
 ) : Logger {
-    fun myPage(member: Member): MyPageResponse {
+    fun getMyPage(member: Member): MyPageDto  {
         checkNotNull(member.id, "MemberId")
         // 회고 만족도, 집중도 평균 조회
-        val (satisfactionAvg, concentrationAvg) = retrospectionService.getRetrospectionAverage(member.id)
+        val retrospectives = retrospectionRepository.findAllByMemberId(member.id)
+        val satisfactionAverage = retrospectives.map { it.satisfaction }.average().toInt()
+        val concentrationAverage = retrospectives.map { it.concentration }.average().toInt()
 
         // 역대 페르소나 조회
-        val personas = personaService.getAllPersonas(member.id)
+        val personas =
+            personaRepository.findPersonasByMemberIdOrderByLatestTask(
+                member.id,
+            ).map { Persona.fromEntity(it) }
 
         // 완료한 일 목록 조회
-        val completedTasks = taskService.getCompletedTasksOrderByCompletedTimeDesc(member.id)
+        val completedTasks =
+            taskRepository.findCompletedTasksOrderByCompletedTimeDesc(
+                member.id,
+            ).map { Task.fromEntity(it) }
 
         // 미룬일 목록 조회
-        val procrastinatedTasks = taskService.getProcrastinatedTasksOrderByDueDateDesc(member.id)
+        val procrastinatedTasks =
+            taskRepository.findProcrastinatedTasksOrderByDueDateDesc(member.id).map {
+                Task.fromEntity(it)
+            }
 
-        // MyPageResponse 반환
-        return MyPageResponse(
-            satisfactionAvg = satisfactionAvg,
-            concentrationAvg = concentrationAvg,
-            personas = personas.map { PersonaDto.fromPersona(it) },
-            completedTasks = completedTasks.map { TaskResponse.fromTask(it) },
-            procrastinatedTasks = procrastinatedTasks.map { TaskResponse.fromTask(it) },
-            completedTaskCount = completedTasks.size,
-            procrastinatedTaskCount = procrastinatedTasks.size,
+        return MyPageDto(
+            satisfactionAvg = satisfactionAverage,
+            concentrationAvg = concentrationAverage,
+            personas = personas,
+            completedTasks = completedTasks,
+            procrastinatedTasks = procrastinatedTasks,
         )
     }
 }
